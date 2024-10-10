3fcefa8eea605190b69fdc32972a891a865c99ff
lucasleja <lejarragalucas@gmail.com>
2024-10-10 13:41:50 -0300

    preEntrega 2 handlebars 0.8

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 import express from 'express';
+import handlebars from 'express-handlebars'
 import cartsRoutes from './routes/carts.route.js'
 import productsRoutes from './routes/products.router.js'
+import __dirname from './utils.js';
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Configurar Handlebars como motor de plantillas
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + "/public"))
+
+
+// Ruta de prueba para Hbs
+app.get('/hello', (req, res) => {
+    // usuario de prueba
+    const usuario = {
+        nombre: 'Lucas',
+        email: 'johndoe@example.com',
+        edad: 30
+    }
+
+    res.render('hello', usuario)
+})
 
 
 const PORT = 8080;
