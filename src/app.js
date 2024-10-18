f628e2d1fe58628304b77c1d8d867a73cc5b1bad
lucasleja <lejarragalucas@gmail.com>
2024-10-18 16:52:49 -0300

    realTimeProducts

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import handlebars from 'express-handlebars'
+import { Server } from 'socket.io'
 import cartsRoutes from './routes/carts.route.js'
 import productsRoutes from './routes/products.router.js'
 import __dirname from './utils.js';
 import viewsRouter from './routes/views.routes.js'
+import ProductManager from './services/ProductManager.js'
 
 
 const app = express();
@@ -29,6 +31,27 @@ app.use('/api/products', productsRoutes)
 app.use('/', viewsRouter)
 
 
-app.listen(PORT, () => {
+/* app.listen(PORT, () => {
 console.log(`Servidor escuchando en el puerto ${PORT}`);
+}) */
+
+const httpServer = app.listen(PORT, ()=>{
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
+
+const io = new Server(httpServer)
+
+const prodManag = new ProductManager()
+
+io.on('connection', async (socket)=>{
+    const products = await prodManag.getAllProducts()
+    socket.emit('productsList', products)
+
+    socket.on('deleting-product', id=>{
+        prodManag.deleteProduct(id)
+    })
+
+    socket.on('new-product', product=>{
+        prodManag.setProduct(product)
+    })
 })
---
+++ src/public/js/realTimeProducts.js
@@ -0,0 +1,60 @@
+const socket = io()
+
+socket.on('productsList', products=>{
+    const productsList = document.getElementById('productsList')
+    let prods = ''
+    products.forEach(prod => {
+        prods += `
+                <div class="product">
+                    <h3>${prod.title}</h3>
+                    <p>${prod.description}</p>
+                    <p>Code: ${prod.code}</p>
+                    <p>Price: $${prod.price}</p>
+                    <p>Stock: ${prod.stock}</p>
+                    <p>Category: ${prod.category}</p>
+                    <button class="btn-delete" id="${prod.id} ">Delete</button>
+                </div>
+                `
+    });
+    productsList.innerHTML = prods
+
+    const btnDeleteProduct = document.querySelectorAll(".btn-delete")
+
+    btnDeleteProduct.forEach(e=>{
+        e.addEventListener("click", (evt)=>{
+            const idProduct = parseInt(evt.target.id)
+            socket.emit('deleting-product', idProduct)
+        })
+    })
+})
+
+const btnForm = document.getElementById("btnForm")
+const inputs = document.querySelectorAll(".input")
+
+let newProduct = {}
+
+inputs.forEach(input=>{
+    input.addEventListener("change", (e)=>{
+        newProduct = {...newProduct, [e.target.name]: e.target.value
+        }
+    })
+})
+
+
+const hidden = document.querySelector('.hidden')
+
+btnForm.addEventListener("click", (e)=>{
+    e.preventDefault()
+    if (!newProduct.title || !newProduct.description || !newProduct.code || !newProduct.price || !newProduct.stock || !newProduct.category) {
+        hidden.classList.remove('hidden')
+    } else {
+        newProduct.stock = parseInt(newProduct.stock)
+        newProduct.price = parseInt(newProduct.price)
+        hidden.classList.add('hidden')
+        socket.emit('new-product', newProduct)
+        newProduct = {}
+        inputs.forEach(input=>{
+            input.value = ""
+        })
+    }
+})
